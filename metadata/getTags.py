1da5a0fce540ba49ec2484ffe1bc6868f672f830
mabarber92 <mathew.barber@aku.edu>
2023-05-11 10:30:50 +0100

    added metadata util - class for getting tags from meta csv and manipulating

---
+++ metadata/getTags.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import re
+
+class getTags():
+    def __init__ (self, meta_csv, col = "tags"):
+        self.meta_df = pd.read_csv(meta_csv, sep="\t")
+        self.fetch_col(col)
+    def fetch_col(self, col="tags"):
+        self.col_series = self.meta_df[col]
+    def list_all_unique(self):
+        return list(dict.fromkeys(self.list_of_all_tags()))
+    def df_all_unique(self, col_name = "unique_tags"):
+        df = pd.DataFrame()
+        df[col_name] = list(dict.fromkeys(self.list_of_all_tags()))
+        return df
+    def list_of_all_tags(self):
+        final_list = []
+        list_of_rows = self.col_series.dropna().tolist()
+        for row in list_of_rows:            
+            tag_list = re.split("\s*::\s+", row)
+            final_list.extend(tag_list)
+        if "" in final_list:
+            print("Space found")
+            final_list = [i for i in final_list if i != " "]
+        return final_list
+    # For future - set up class so obj can be filtered like it is a dataframe
