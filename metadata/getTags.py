48381a5fbd8a13d8df4b332f8e22dec6bd3734d7
mabarber92 <mathew.barber@aku.edu>
2023-05-11 15:00:49 +0100

    added alphabetical sort to unique list and df

--- metadata/getTags.py
+++ metadata/getTags.py
@@ -8,11 +8,13 @@ class getTags():
     def fetch_col(self, col="tags"):
         self.col_series = self.meta_df[col]
     def list_all_unique(self):
-        return list(dict.fromkeys(self.list_of_all_tags()))
+        return self.get_unique_sort().tolist()
     def df_all_unique(self, col_name = "unique_tags"):
+        return self.get_unique_sort(col_name)
+    def get_unique_sort(self, col_name = "unique_tags"):
         df = pd.DataFrame()
         df[col_name] = list(dict.fromkeys(self.list_of_all_tags()))
-        return df
+        return df.sort_values(by = col_name)
     def list_of_all_tags(self):
         final_list = []
         list_of_rows = self.col_series.dropna().tolist()
